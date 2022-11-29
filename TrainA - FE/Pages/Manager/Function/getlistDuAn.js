18360d15bc25361b3b6c99771d6cde4a96219614
Nguyen Huy Anh Thư <63990707+NguyenHuyAnhThu@users.noreply.github.com>
2022-11-29 20:57:35 +0700

    example du an integrate

--- TrainA - FE/Pages/Manager/Function/getlistDuAn.js
+++ TrainA - FE/Pages/Manager/Function/getlistDuAn.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
   var apiURL = "https://localhost:44398/api/duan";
   $.ajax({
     method: "GET",
-    headers: {
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Content-Type": "application/json",
-    },
-    crossDomain: true,
-    dataType: "jsonp",
     url: apiURL,
     success: function (res) {
       console.log("hello", res);
+      var result = "";
+      for (let i = 0; i < res.length; i++) {
+        result += `<tr>
+          <td>${res[i].MA_DA}</td>
+          <td>${res[i].TEN_DA}</td>
+		  <td>${res[i].NGAYBATDAU}</td>
+		  <td>${res[i].NGAYKETTHUC}</td>
+		  <td>${res[i].NOIDUNG}</td>
+        </tr>`;
+      }
+      $(`#project_list`).append(result);
     },
     error: function (err) {
       console.log("error", err);
